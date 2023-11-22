14da3bb2e1fd06f2fc57e78c381b88b1281466f9
Junior flores <fmjg21@gmail.com>
2023-11-22 14:54:57 +0700

    parts and props building

--- data-scripts/cvat/attach_data.py
+++ data-scripts/cvat/attach_data.py
@@ -12,6 +12,7 @@ from utils.index import (
 from tqdm import tqdm
 import fire
 import geopandas as gpd
+from copy import deepcopy
 
 BUILDING_PROPERTIES = [
     "box_attr_building_condition",
@@ -21,7 +22,7 @@ BUILDING_PROPERTIES = [
     "box_attr_building_completeness",
 ]
 
-BUILDING_VALUES = {
+BUILDING_PROPS = {
     "brick_or_cement-concrete_block": 1,
     "plaster": 2,
     "wood_polished": 3,
@@ -45,23 +46,36 @@ BUILDING_VALUES = {
     "good": 21,
 }
 
+BUILDING_PARTS = {
+    "window": 1,
+    "door": 2,
+    "garage": 3,
+    "disaster_mitigation": 4
+}
+
 
 def combine_resources(
-    cvat_csv,
-    original_geojson,
-    geojson_output,
-    csv_output_trajectory,
-    props_inference_file,
-    props_map_file,
-    gpkg_buildings_file,
-    shp_buildings_file,
+        annotation_properties_csv,
+        annotation_parts_csv,
+        original_geojson,
+        gpkg_buildings_file,
+        geojson_merge_output,
+        shp_buildings_file,
+        csv_output_trajectory,
+        props_inference_file,
+        props_map_file,
+        parts_inference_file,
+        parts_map_file,
 ):
     features = read_geojson(original_geojson)
-    csv_data = read_csv(cvat_csv)
+    csv_data_props = read_csv(annotation_properties_csv)
+    csv_data_parts = read_csv(annotation_parts_csv)
+
+    list_data_annotation = [*csv_data_props, *csv_data_parts]
     df_polygons = gpd.read_file(gpkg_buildings_file)
     # group csv data
     csv_groups = {}
-    for box in tqdm(csv_data, desc="join data"):
+    for box in tqdm(list_data_annotation, desc="join data"):
         fake_key = f'{box["img_path"]}/{box["img_name"]}'
         if not csv_groups.get(fake_key):
             csv_groups[fake_key] = []
@@ -72,11 +86,11 @@ def combine_resources(
         fake_key = "/".join(props.get("image_path", "").split("/")[-3:])
         props["box"] = csv_groups.get(fake_key, [])
 
-    write_geojson(geojson_output, features)
+    write_geojson(geojson_merge_output, features)
     # housing_passports formats
     trajectories_out = []
-    # building_parts = []
     building_props_out = []
+    building_parts_out = []
 
     for feature in tqdm(features, desc="Hp  create format"):
         props = feature.get("properties", {})
@@ -94,7 +108,7 @@ def combine_resources(
             "neighborhood": "n1",
             "subfolder": "/".join(["data", *path_seq[-3:-1]]),
         }
-        box_building_props = {
+        box_props = {
             "detection_scores": [],
             "detection_classes": [],
             "detection_boxes": [],
@@ -104,6 +118,9 @@ def combine_resources(
             "frame": image_name.split(".")[0],
             "neighborhood": "n1",
         }
+        box_building_props = deepcopy(box_props)
+        box_building_parts = deepcopy(box_props)
+
         trajectories_out.append(trajectory)
 
         for box_meta in props.get("box", []):
@@ -113,19 +130,33 @@ def combine_resources(
                 float(box_meta.get("box_xbr", "")),
                 float(box_meta.get("box_ybr", "")),
             ]
-            for prop_key in BUILDING_PROPERTIES:
-                if box_meta.get(prop_key):
-                    new_key = BUILDING_VALUES.get(box_meta.get(prop_key))
-                    box_building_props["detection_boxes"].append(box)
-                    box_building_props["detection_classes"].append(new_key)
 
-        building_props_out.append(box_building_props)
-    # write_json(BUILDING_VALUES, )
+            box_label = box_meta.get("box_label")
+            if not box_label:
+                continue
+            if box_label == "building_properties":
+                for prop_key in BUILDING_PROPERTIES:
+                    if box_meta.get(prop_key):
+                        new_key = BUILDING_PROPS.get(box_meta.get(prop_key))
+                        box_building_props["detection_boxes"].append(deepcopy(box))
+                        box_building_props["detection_classes"].append(new_key)
+            else:
+                new_key = BUILDING_PARTS.get(box_label)
+                box_building_parts["detection_boxes"].append(deepcopy(box))
+                box_building_parts["detection_classes"].append(new_key)
+
+        building_props_out.append(deepcopy(box_building_props))
+        building_parts_out.append(deepcopy(box_building_parts))
+
+    # write_json(BUILDING_PROPS, )
     # write files
     write_dictlist2csv(csv_output_trajectory, trajectories_out)
     # write props
     write_json(props_inference_file, building_props_out)
-    write_pbtxt_content(props_map_file, BUILDING_VALUES)
+    write_json(parts_inference_file, building_parts_out)
+
+    write_pbtxt_content(props_map_file, BUILDING_PROPS)
+    write_pbtxt_content(parts_map_file, BUILDING_PARTS)
 
     # write shp
     df_polygons["neighborhood"] = "n1"
--- data-scripts/post_processing.sh
+++ data-scripts/post_processing.sh
@@ -4,31 +4,41 @@ set -e
 export AWS_PROFILE=yunica-hp
 
 dataOut=data
-shpOut=$dataOut/shp_building
+dataPrepared=$dataOut/files_for_db
+shpOut=$dataPrepared/shp_building
 
 mkdir -p $shpOut
 
 geokitpy="docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:python.latest"
 
 # download data
-# aws s3 cp s3://hp-images-v2/mapillary_files/map_points__pano_clip.geojson $dataOut/map_points__pano_clip.geojson
-
-
-# convert xml to csv
-# $geokitpy cvat xml2csv \
-#   --xml_file=$dataOut/annotations.xml \
-#   --csv_file=$dataOut/annotations.csv \
-#   --full=True
+#aws s3 cp s3://hp-images-v2/mapillary_files/map_points__pano_clip.geojson $dataOut/map_points__pano_clip.geojson
+#aws s3 cp s3://hp-deliverables-v2/parts_left.xml $dataOut/parts.xml
+#aws s3 cp s3://hp-deliverables-v2/properties_left.xml $dataOut/properties.xml
+#aws s3 cp s3://hp-images-v2/mapillary_files/bldgs_combined.gpkg $dataOut/bldgs_combined.gpkg
 #
+## convert xml to csv
+#$geokitpy cvat xml2csv \
+#  --xml_file=$dataOut/properties.xml \
+#  --csv_file=$dataOut/properties.csv \
+#  --full=True
+#
+#$geokitpy cvat xml2csv \
+#  --xml_file=$dataOut/parts.xml \
+#  --csv_file=$dataOut/parts.csv \
+#  --full=True
 
 attach_cvat_data \
-  $dataOut/annotations.csv \
+  $dataOut/properties.csv \
+  $dataOut/parts.csv \
   $dataOut/map_points__pano_clip.geojson \
-  $dataOut/annotation_merge.geojson \
-  $dataOut/trajectory.csv \
-  $dataOut/props_inference_file.json \
-  $dataOut/props_map_file.pbtxt \
   $dataOut/bldgs_combined.gpkg \
-  $shpOut/bldgs_combined.shp
+  $dataPrepared/annotation_merge.geojson \
+  $shpOut/shp_building.shp \
+  $dataPrepared/trajectory.csv \
+  $dataPrepared/props_inference_file.json \
+  $dataPrepared/props_map_file.pbtxt \
+  $dataPrepared/parts_inference_file.json \
+  $dataPrepared/parts_map_file.pbtxt
 
-
+#aws s3 sync $dataPrepared s3://hp-images-v2/data_prepared/
