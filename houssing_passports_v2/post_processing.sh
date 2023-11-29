9468689464666178c922f52747f262621f714517
Junior flores <fmjg21@gmail.com>
2023-11-29 17:52:24 +0200

    update bash

--- houssing_passports_v2/post_processing.sh
+++ houssing_passports_v2/post_processing.sh
@@ -9,7 +9,7 @@ shpOut=$dataPrepared/shp_building
 
 mkdir -p $shpOut
 
-geokitpy="docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:python.latest"
+#geokitpy="docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:python.latest"
 
 # download data
 #aws s3 cp s3://hp-images-v2/mapillary_files/map_points__pano_clip.geojson $dataOut/map_points__pano_clip.geojson
@@ -40,6 +40,8 @@ attach_cvat_data \
   $dataPrepared/props_map_file.pbtxt \
   $dataPrepared/parts_inference_file.json \
   $dataPrepared/parts_map_file.pbtxt \
-  /Developer/housing-passports-v2/data-scripts/data/images
+  /Users/juniorflores/Developer/housing-passports-v2/houssing_passports_v2/data/images \
+  $dataPrepared/properties_key.json \
+  $dataPrepared/parts_key.json
 
 #aws s3 sync $dataPrepared s3://hp-images-v2/data_prepared/
